e10cbe7a6be051dc5e30338c642919f7f433cdbc
BradleyHu0707 <bradleyhu0707@gmail.com>
2024-06-07 08:20:53 -0600

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,1 +1,8 @@
-
+let userResponse = confirm("Do you want to continue?");
+if (userResponse) {
+    let password = prompt("Please enter the password:");
+        
+    if (password === '1234') {
+        alert('Successful!');
+    }
+}
